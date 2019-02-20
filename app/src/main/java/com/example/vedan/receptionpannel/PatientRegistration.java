1513b2dfe543cd4b327c3b2ee253e42838fcb642
Vedang Barhate <dvedang4u@gmail.com>
2019-02-20 20:49:13 +0530

    Firebase added and Server Operable

--- app/src/main/java/com/example/vedan/receptionpannel/PatientRegistration.java
+++ app/src/main/java/com/example/vedan/receptionpannel/PatientRegistration.java
@@ -2,24 +2,29 @@ package com.example.vedan.receptionpannel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class PatientRegistration extends AppCompatActivity {
 
-    EditText mobileno;
+    public EditText mobileno;
+    private DatabaseReference databaseReference;
 
     public void registerHere(View view)
     {
-        mobileno = (EditText) findViewById(R.id.mobileIDText);
+        mobileno = findViewById(R.id.mobileIDText);
         Toast.makeText(this, "Message sent to "+mobileno.getText().toString(), Toast.LENGTH_SHORT).show();
+        databaseReference.child("Mobile no:-").setValue(mobileno.getText().toString());
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_registration);
-    }
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+}
 }
